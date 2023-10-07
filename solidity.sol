404e22911869c5e442edd98441989f25d2d594ed
LunarEchoes <127887784+LunarEchoesABC@users.noreply.github.com>
2023-10-07 19:35:46 +0200

    Update solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -1,40 +1,11 @@
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
-uint256 x = 123;
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.20 and less than 0.9.0
+pragma solidity ^0.8.20;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+    string public greet = "Hello World!";
+    string public greet = "Hello World!";
+    string public greet = "Hello World!";
+    string public greet = "Hello World!";
+}
