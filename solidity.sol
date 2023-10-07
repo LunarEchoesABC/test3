77b2ce0c3f414674aab6f39f4c61285338e5983b
LunarEchoes <127887784+LunarEchoesABC@users.noreply.github.com>
2023-10-08 00:49:33 +0200

    Update solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -4,6 +4,5 @@ pragma solidity ^0.8.20;
 
 contract HelloWorld {
     string public greet = "Hello World!";
-    string public greet = "Hello World!";
-    uint256 greet = 123;
+    string public greet = "Hello World!"
 }
